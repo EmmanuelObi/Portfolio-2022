4943169dafe79c74197ec0479dd85d4687d5155a
EmmanuelObi <obiemmy123@gmail.com>
2022-07-29 22:07:21 +0100

    added info for SEO practices

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import { Container } from "./LayoutStyles";
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
-
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
   return (
-    <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
-    </Container>
-  )
-}
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta
+          name="description"
+          content="Portfolio website developed by Emmanuel, containing projects, skills and more about educational and professional background."
+        />
+         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <title>Emmanuel Obi - Portfolio</title>
+      </Head>
+      <Container>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </Container>
+    </>
+  );
+};
